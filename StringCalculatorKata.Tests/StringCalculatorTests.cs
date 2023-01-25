4118c3b747c367557886a509b3313ebe32969eaa
duaa-braik <duaa13braik@gmail.com>
2023-01-26 00:47:38 +0200

    add test method for single-number string

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -3,6 +3,7 @@ namespace StringCalculatorKata.Tests
     public class StringCalculatorTests
     {
         private StringCalculator StringCalculator { get; }
+        private int Output { get; set; }
 
         public StringCalculatorTests()
         {
@@ -12,9 +13,17 @@ namespace StringCalculatorKata.Tests
         [Fact]
         public void ShouldReturnZeroIfEmpty()
         {
-            int Output = StringCalculator.Add("");
+            Output = StringCalculator.Add("");
 
             Assert.Equal(0, Output);
+        }
+
+        [Fact]
+        public void ShouldReturnTheSameNumber()
+        {
+            Output = StringCalculator.Add("1");
+
+            Assert.Equal(1, Output);
         }
     }
 }
