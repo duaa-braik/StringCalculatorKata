686d11d82998ae4bc899a2e05dc9d84f9b964ac8
duaa-braik <duaa13braik@gmail.com>
2023-01-26 00:33:59 +0200

    add test method for empty string

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -2,10 +2,19 @@ namespace StringCalculatorKata.Tests
 {
     public class StringCalculatorTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private StringCalculator StringCalculator { get; }
 
+        public StringCalculatorTests()
+        {
+            StringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void ShouldReturnZeroIfEmpty()
+        {
+            int Output = StringCalculator.Add("");
+
+            Assert.Equal(0, Output);
         }
     }
 }
