28a58a1e7766e4f08cffa492bd42179db68d5938
duaa-braik <duaa13braik@gmail.com>
2023-01-26 23:28:01 +0200

    Splitted The Test Method into Multiple tests

--- StringCalculatorKata.Tests/StringCalculatorTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -12,25 +12,29 @@ namespace StringCalculatorKata.Tests
 
         [Theory]
         [InlineData("", 0)]
+        public void ShouldReturnZeroForEmptyString(string Input, int Expected)
+        {
+            Output = StringCalculator.Add(Input);
+            Assert.Equal(Expected, Output);
+        }
+
+        [Theory]
         [InlineData("1", 1)]
+        public void ShouldReturnTheSameNumber_ForSequenceWithOneNumber(string Input, int Expected)
+        {
+            Output = StringCalculator.Add(Input);
+            Assert.Equal(Expected, Output);
+        }
+
+        [Theory]
         [InlineData("1,2,3,4", 10)]
         [InlineData("1\n2,3", 6)]
         [InlineData("//;\n1;2", 3)]
+        [InlineData("//!\n1!2", 3)]
         public void ShouldReturnSumOfTheSumOfNumbers(String input, int Expected)
         {
             Output = StringCalculator.Add(input);
-            if (string.IsNullOrEmpty(input))
-            {
-                Assert.Equal(Expected, Output);
-            }
-            else if (input.Length == 1)
-            {
-                Assert.Equal(Expected, Output);
-            }
-            else if (input.Length > 1)
-            {
-                Assert.Equal(Expected, Output);
-            }
+            Assert.Equal(Expected, Output);
         }
     }
 }
